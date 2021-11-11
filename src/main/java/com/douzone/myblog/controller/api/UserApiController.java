6c4b0952f43eefa5876c0ab3fd912c904c040ef4
dhcho <oop6413@gmail.com>
2021-11-11 22:20:08 +0900

    #48 Spring Security practice

--- src/main/java/com/douzone/myblog/controller/api/UserApiController.java
+++ src/main/java/com/douzone/myblog/controller/api/UserApiController.java
@@ -1,7 +1,5 @@
 package com.douzone.myblog.controller.api;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,9 +17,6 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
-	@Autowired
-	private HttpSession session;
-	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) { // username, password, email
 		System.out.println("UserApiController : save 호출됨");
@@ -32,14 +27,14 @@ public class UserApiController {
 	}
 	
 	// 다음시간에 스프링 시큐리티 이용해서 로그인!!
-	@PostMapping("/api/user/login")
-	public ResponseDto<Integer> login(@RequestBody User user) {
-		System.out.println("User ApiController : login호출됨");
-		User principal = userService.login(user); // principal (접근주체)
-		
-		if(principal != null) {
-			session.setAttribute("principal", principal);
-		}
-		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
-	}
+	//	@PostMapping("/api/user/login")
+	//	public ResponseDto<Integer> login(@RequestBody User user, HttpSession session) {
+	//		System.out.println("User ApiController : login호출됨");
+	//		User principal = userService.login(user); // principal (접근주체)
+	//		
+	//		if(principal != null) {
+	//			session.setAttribute("principal", principal);
+	//		}
+	//		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+	//	}
 }
