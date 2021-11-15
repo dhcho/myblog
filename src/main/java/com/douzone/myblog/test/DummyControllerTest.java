98802b903ceabd7367acbe26fc33e680d6980a70
dhcho <oop6413@gmail.com>
2021-11-15 23:51:48 +0900

    #55 paging : JPA practice

--- src/main/java/com/douzone/myblog/test/DummyControllerTest.java
+++ src/main/java/com/douzone/myblog/test/DummyControllerTest.java
@@ -73,11 +73,11 @@ public class DummyControllerTest {
 	// http://localhost:8000/blog/dummy/user
 	// http://localhost:8000/blog/dummy/user?page=0
 	@GetMapping("/dummy/user")
-	public List<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC) Pageable pageable) {
+	public Page<User> pageList(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC) Pageable pageable) {
 		Page<User> pagingUser = userRepository.findAll(pageable);
 		
 		List<User> users = pagingUser.getContent();
-		return users;
+		return pagingUser;
 	}
 	
 	// {id} 주소로 파라미터를 전달 받을 수 있음.
