5a34a3ef68e73e83c0246779d7f8db2d0b463620
dhcho <oop6413@gmail.com>
2021-11-15 22:34:22 +0900

    #54 list : JPA practice

--- src/main/java/com/douzone/myblog/controller/BoardController.java
+++ src/main/java/com/douzone/myblog/controller/BoardController.java
@@ -1,15 +1,24 @@
 package com.douzone.myblog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.douzone.myblog.service.BoardService;
 
 @Controller
 public class BoardController {
 	
+	@Autowired
+	private BoardService boardService;
+	
+	// 컨트롤러에서 세션을 어떻게 찾는지?
+	// @AuthenticationPrincipal PrincipalDetail principal
 	@GetMapping({"", "/"})
-	public String index() { // 컨트롤러에서 세션을 어떻게 찾는지?
-		// /WEB-INF/views/index.jsp
-		return "index";
+	public String index(Model model) { 
+		model.addAttribute("boards", boardService.list());
+		return "index"; // viewResolver 작동!!
 	}
 	
 	// USER 권한이 필요
--- src/main/java/com/douzone/myblog/service/BoardService.java
+++ src/main/java/com/douzone/myblog/service/BoardService.java
@@ -1,5 +1,7 @@
 package com.douzone.myblog.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,5 +21,9 @@ public class BoardService {
 		board.setCount(0);
 		board.setUser(user);
 		boardRepository.save(board);
+	}
+	
+	public List<Board> list() {
+		return boardRepository.findAll();
 	}
 }
