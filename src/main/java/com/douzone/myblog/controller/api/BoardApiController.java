6fc404f844ccde2d6e908aa5d70eb62f4ca09f4b
dhcho <oop6413@gmail.com>
2022-09-08 21:39:54 +0900

    #68 reply write

--- src/main/java/com/douzone/myblog/controller/api/BoardApiController.java
+++ src/main/java/com/douzone/myblog/controller/api/BoardApiController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.douzone.myblog.config.auth.PrincipalDetail;
 import com.douzone.myblog.dto.ResponseDto;
 import com.douzone.myblog.model.Board;
+import com.douzone.myblog.model.Reply;
 import com.douzone.myblog.service.BoardService;
 
 @RestController
@@ -36,6 +37,13 @@ public class BoardApiController {
 	@PutMapping("/api/board/{id}")
 	public ResponseDto<Integer> update(@PathVariable int id, @RequestBody Board board) {
 		boardService.update(id, board);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+	}
+	
+	@PostMapping("/api/board/{boardId}/reply")
+	public ResponseDto<Integer> replySave(@PathVariable int boardId, @RequestBody Reply reply, @AuthenticationPrincipal PrincipalDetail principal) {
+		
+		boardService.replyWrite(principal.getUser(), boardId, reply);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
 	}
 }
--- src/main/java/com/douzone/myblog/service/BoardService.java
+++ src/main/java/com/douzone/myblog/service/BoardService.java
@@ -7,14 +7,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.douzone.myblog.model.Board;
+import com.douzone.myblog.model.Reply;
 import com.douzone.myblog.model.User;
 import com.douzone.myblog.repository.BoardRepository;
+import com.douzone.myblog.repository.ReplyRepository;
 
 @Service
 public class BoardService {
 
 	@Autowired
 	private BoardRepository boardRepository;
+	
+	@Autowired
+	private ReplyRepository replyRepository;
 	
 	@Transactional
 	public void write(Board board, User user) { // title, content
@@ -50,5 +55,17 @@ public class BoardService {
 		board.setTitle(requestBoard.getTitle());
 		board.setContent(requestBoard.getContent());
 		// 해당 함수로 종료시(Service가 종료될 때) 트랜잭션이 종료됩니다. 이때 더티체킹 - 자동 업데이트가 됨. db flush
+	}
+	
+	@Transactional
+	public void replyWrite(User user, int boardId, Reply requestReply) {
+		Board board = boardRepository.findById(boardId).orElseThrow(()->{
+			return new IllegalArgumentException("댓글 쓰기 실패 : 게시글 ID를 찾을 수 업습니다.");
+		}); // 영속화 완료
+		
+		requestReply.setUser(user);
+		requestReply.setBoard(board);
+		
+		replyRepository.save(requestReply);
 	}
 }
