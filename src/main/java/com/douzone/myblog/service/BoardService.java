46111143ec55fe6d0c49b26050e70d37e5a58cc1
dhcho <oop6413@gmail.com>
2022-09-09 22:53:58 +0900

    #73 reply delete FIN

--- src/main/java/com/douzone/myblog/service/BoardService.java
+++ src/main/java/com/douzone/myblog/service/BoardService.java
@@ -87,4 +87,9 @@ public class BoardService {
 		int result = replyRepository.mSave(replySaveRequestDto.getUserId(), replySaveRequestDto.getBoardId(), replySaveRequestDto.getContent());
 		System.out.println("BoardService : " + result);
 	}
+	
+	@Transactional
+	public void replyDelete(int replyId) {
+		replyRepository.deleteById(replyId);
+	}
 }
